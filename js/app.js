5cb35617ac597606d4229dba87754dd5e8949edd
Sean Jun <seanjun21@gmail.com>
2016-06-15 14:35:02 -0700

    Created newGame and theGuess function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,27 +11,49 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $("#guessButton").on('click', function(event) {
+        event.preventDefault();
+        theGuess();
+    })
+
+    //New Game
+    var newGame = function() {
+        //Computer generates random number (1 - 100)
+    	var randomNumber = Math.floor((Math.random() * 100) + 1);
+        return randomNumber;
+    }
+
+    newGame();
 
 
+    //The Game
+    var theGuess = function() {
+        var userNumber = $("#userGuess").val();	
+        var distance = Math.abs(userNumber - randomNumber);
 
-  	var newGame = function() {
-  	//Computer generates random number (1 - 100)
-  		var randomNumber = Math.floor((Math.random() * 100) + 1);
+        console.log(distance);
 
-  	//Users input
-  		var userGuess = function() {
-			var userNumber = $("#userGuess").val();	
+    	if (userNumber <= 100 && userNumber >= 1) {
+    		if (distance === 0) {
+    			console.log("You got it!");
+    		} else if (distance <= 5) {
+                console.log("Burning");
+            } else if (distance <= 15) {
+                console.log("Hot");
+            } else if (distance <= 25) {
+                console.log("Warm");
+            } else if (distance <= 50) {
+                console.log("Cold");
+            } else if (distance <= 99) {
+                console.log("Freezing");
+            }
+	    }
+    }
 
-			if (userNumber <= 100 && userNumber >= 1) {
-				if (userNumber == randomNumber) {
-					console.log("You got it!");
-				}
-			}
-		}
-  	}
+
 });
 
-newGame();
+
 
 
 
