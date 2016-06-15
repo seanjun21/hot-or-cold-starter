76902f15d7ae589a3f77aa836d355312b3fe56b7
Eric Snell <eric_snell21@yahoo.com>
2016-06-15 16:37:26 -0400

    switch

--- js/app.js
+++ js/app.js
@@ -12,6 +12,27 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+
+  	var newGame = function() {
+  	//Computer generates random number (1 - 100)
+  		var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+  	//Users input
+  		var userGuess = function() {
+			var userNumber = $("#userGuess").val();	
+
+			if (userNumber <= 100 && userNumber >= 1) {
+				if (userNumber == randomNumber) {
+					console.log("You got it!");
+				}
+			}
+		}
+  	}
 });
+
+newGame();
+
 
 
